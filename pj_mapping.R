5405e424ca7cb94e89b30997db13fe93d9e4de37
Sebastian Lopez-Marcano <slopezmarcano@gmail.com>
2023-02-06 09:20:32 +1000

    minimum convex polygon and add to the leaflet map #1

--- pj_mapping.R
+++ pj_mapping.R
@@ -1,16 +1,20 @@
 #Script to plot a dynamic map using ALA API
+#SLM
+#Created:
+#Updated: 06022023
+
 
 #-- LIBRARIES --#
-library(leaflet)
-library(rjson)
-library(jsonlite)
-library(httr)
-library(tidyverse)
-library(viridis)
+library(leaflet) #mapping 
+library(rjson) #json wrangling and requests
+library(jsonlite) #json wrangling
+library(httr) #http request
+library(tidyverse) #data wrangling
+library(viridis) #colour scheme
 
 # -- API REQUEST --#
 #Port Jackson Shark
-link <- "https://biocache-ws.ala.org.au/ws/occurrences/search?q=taxa%3A%22Port%20Jackson%20Shark%22&qualityProfile=ALA&pageSize=10000" #TODO: #3 the extraction of all data can be performed via a function and loop
+link <- "https://biocache-ws.ala.org.au/ws/occurrences/search?q=taxa%3A%22Port%20Jackson%20Shark%22&qualityProfile=ALA&pageSize=40000" #TODO: #3 the extraction of all data can be performed via a function and loop
 
 #Get data
 response <- GET(link)
@@ -27,16 +31,18 @@ pj_cleaned <- pjdata %>%
 
 
 # -- MINIMUM CONVEX POLYGON --#
-#TODO: #1 minimum convex polygon and add to the leaflet map
+coords <- cbind(pj_cleaned$decimalLongitude,pj_cleaned$decimalLatitude) %>%
+          na.omit()
 
-#coords <- cbind(pj_cleaned$decimalLongitude,pj_cleaned$decimalLatitude)
-#hull <- chull(coords)
-#polygon <- coords[hull,]
+hull <- chull(coords)
+polygons <- as.data.frame(coords[hull,])
 
 # -- DEFINE PALETTE FOR MAP --#
 palettini <- colorFactor(viridis(7), pj_cleaned$basisOfRecord)
 
-leaflet(pj_cleaned) %>%
+show_polygon <- TRUE
+
+map <- leaflet(pj_cleaned) %>%
  addProviderTiles(providers$CartoDB.Positron, group = "Positron")%>%
   addProviderTiles(providers$OpenStreetMap)%>%
   addProviderTiles(providers$CartoDB.DarkMatter, group = "DarkMatter") %>%
@@ -47,8 +53,13 @@ leaflet(pj_cleaned) %>%
                   stroke = FALSE,
                   fillOpacity = 0.8,
                   popup = ~dataProviderName) %>%
+  addPolygons(lat = polygons$V2, lng = polygons$V1, 
+    color = "red", opacity = 0.5, fillOpacity = 0.3,
+    group = "Convex Hull",
+    layerId = "convex-hull") %>%
   addLegend("bottomright", pal = palettini, values = ~basisOfRecord, 
-  labels = "Basis of Records", title = "Map Replication from ALA") %>%
+    labels = "Basis of Records", title = "Map Replication from ALA") %>%
   addLayersControl(
-    baseGroups = c("OpenStreetMap", "Positron", "DarkMatter"),
-  options = layersControlOptions(collapsed = TRUE))
+    baseGroups = c("OpenStreetMap", "Positron", "DarkMatter", "Minimum Convex Hull"),
+    options = layersControlOptions(collapsed = TRUE))
+
